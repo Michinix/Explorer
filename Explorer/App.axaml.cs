786c49646e2bb9d412bbe4ba769d7211dcd5859d
Michinix <michel_marcotte@orange.fr>
2026-05-23 16:22:51 +0200

    FEAT: Implement dependency injection for view models and services

--- Explorer/App.axaml.cs
+++ Explorer/App.axaml.cs
@@ -1,16 +1,19 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
-using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using Explorer.Models;
+using Explorer.Services;
 using Explorer.ViewModels;
 using Explorer.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Explorer;
 
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; } = null!;
+    
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,11 +21,20 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<Browser>();
+        services.AddSingleton<BrowserService>();
+        services.AddSingleton<HomeViewModel>();
+        services.AddSingleton<MainWindowViewModel>();
+
+        Services = services.BuildServiceProvider();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = Services.GetRequiredService<MainWindowViewModel>()
             };
         }
 
