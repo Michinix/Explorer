4683aad84b0684d73a53035740b4518a1dfd872e
Michinix <michel_marcotte@orange.fr>
2026-06-07 16:20:09 +0200

    FEAT: Update size formatting in FileSystemService to use binary prefixes

--- Explorer/Services/FileSystemService.cs
+++ Explorer/Services/FileSystemService.cs
@@ -41,8 +41,10 @@ public static class FileSystemService
 
     private static string FormatSize(long bytes) => bytes switch
     {
-        < 1024        => $"{bytes} B",
-        < 1024 * 1024 => $"{bytes / 1024.0:F1} KB",
-        _             => $"{bytes / (1024.0 * 1024):F1} MB"
+        < 1_024L                    => $"{bytes} o",
+        < 1_024L * 1_024            => $"{bytes / 1_024.0:F1} Ko",
+        < 1_024L * 1_024 * 1_024    => $"{bytes / (1_024.0 * 1_024):F1} Mo",
+        < 1_024L * 1_024 * 1_024 * 1_024 => $"{bytes / (1_024.0 * 1_024 * 1_024):F1} Go",
+        _                           => $"{bytes / (1_024.0 * 1_024 * 1_024 * 1_024):F1} To"
     };
 }
