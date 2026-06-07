18fe615914a4f912367eb204217d324053e76b6e
Michinix <michel_marcotte@orange.fr>
2026-06-07 16:26:56 +0200

    REFAC: Remove byte size formatting for values less than 1 KB in FileSystemService

--- Explorer/Services/FileSystemService.cs
+++ Explorer/Services/FileSystemService.cs
@@ -41,7 +41,6 @@ public static class FileSystemService
 
     private static string FormatSize(long bytes) => bytes switch
     {
-        < 1_024L                    => $"{bytes} o",
         < 1_024L * 1_024            => $"{bytes / 1_024.0:F1} Ko",
         < 1_024L * 1_024 * 1_024    => $"{bytes / (1_024.0 * 1_024):F1} Mo",
         < 1_024L * 1_024 * 1_024 * 1_024 => $"{bytes / (1_024.0 * 1_024 * 1_024):F1} Go",
