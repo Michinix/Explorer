285fddfd5566027b25dc82caf1c12aa460f58ed2
Michinix <michel_marcotte@orange.fr>
2026-06-10 12:48:13 +0200

    REFAC: Simplify service provider initialization in App class

--- Explorer/App.axaml.cs
+++ Explorer/App.axaml.cs
@@ -5,14 +5,11 @@ using Explorer.Services;
 using Explorer.ViewModels;
 using Explorer.Views;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Explorer;
 
 public class App : Application
 {
-    public IServiceProvider? Services { get; private set; }
-
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -21,19 +18,19 @@ public class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         var services = new ServiceCollection();
-    
+
         services.AddSingleton<NavigationService>();
         services.AddSingleton<MainWindowViewModel>();
         services.AddTransient<HomeViewModel>();
         services.AddTransient<SplashViewModel>();
 
-        Services = services.BuildServiceProvider();
+        var provider = services.BuildServiceProvider();
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = Services.GetRequiredService<MainWindowViewModel>()
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
             };
         }
 
