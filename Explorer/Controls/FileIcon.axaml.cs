ba84f4449bdccef9a37a46ea1241576523843bbe
Michinix <michel_marcotte@orange.fr>
2026-06-20 15:21:53 +0200

    FEAT: Add FileIcon control for dynamic file type representation in DataGrid and AsideRight

---
+++ Explorer/Controls/FileIcon.axaml.cs
@@ -0,0 +1,40 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Explorer.Controls;
+
+public partial class FileIcon : UserControl
+{
+    public static readonly StyledProperty<IBrush?> IconBrushProperty =
+        AvaloniaProperty.Register<FileIcon, IBrush?>(nameof(IconBrush));
+
+    public static readonly StyledProperty<string?> IconLabelProperty =
+        AvaloniaProperty.Register<FileIcon, string?>(nameof(IconLabel));
+
+    public static readonly StyledProperty<bool> IsDirectoryProperty =
+        AvaloniaProperty.Register<FileIcon, bool>(nameof(IsDirectory));
+
+    public IBrush? IconBrush
+    {
+        get => GetValue(IconBrushProperty);
+        set => SetValue(IconBrushProperty, value);
+    }
+
+    public string? IconLabel
+    {
+        get => GetValue(IconLabelProperty);
+        set => SetValue(IconLabelProperty, value);
+    }
+
+    public bool IsDirectory
+    {
+        get => GetValue(IsDirectoryProperty);
+        set => SetValue(IsDirectoryProperty, value);
+    }
+
+    public FileIcon()
+    {
+        InitializeComponent();
+    }
+}
