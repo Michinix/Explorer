f49325a052f3145cf2eee62683b3e08e65aa6ca1
Michinix <michel_marcotte@orange.fr>
2026-06-23 17:07:55 +0200

    FEAT: Implement AsideLeft control with navigation commands

---
+++ Explorer/ViewModels/AsideLeftViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using CommunityToolkit.Mvvm.Input;
+using Explorer.Services;
+
+namespace Explorer.ViewModels;
+
+public partial class AsideLeftViewModel(NavigationService navigation) : ViewModelBase
+{
+    [RelayCommand]
+    private void NavigateToDesktop() =>
+        navigation.NavigateTo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+    [RelayCommand]
+    private void NavigateToDownloads()
+    {
+        var downloadsPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads"
+        );
+        navigation.NavigateTo(downloadsPath);
+    }
+
+    [RelayCommand]
+    private void NavigateToDocuments() =>
+        navigation.NavigateTo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+    [RelayCommand]
+    private void NavigateToPictures() =>
+        navigation.NavigateTo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+}
--- Explorer/ViewModels/HomeViewModel.cs
+++ Explorer/ViewModels/HomeViewModel.cs
@@ -4,6 +4,7 @@ namespace Explorer.ViewModels;
 
 public class HomeViewModel(NavigationService navigation) : ViewModelBase
 {
-    public DataGridViewModel DataGrid { get; } = new(navigation);
     public NavBarViewModel NavBar { get; } = new(navigation);
+    public AsideLeftViewModel AsideLeft { get; } = new(navigation);
+    public DataGridViewModel DataGrid { get; } = new(navigation);
 }
