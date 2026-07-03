29f5e9d437f667a5380c2da3f11dd0f4e9c742d0
Michinix <michel_marcotte@orange.fr>
2026-07-03 21:50:19 +0200

    FEAT: Add ListDrivesAsync method

--- Explorer/Services/FileSystemService.cs
+++ Explorer/Services/FileSystemService.cs
@@ -9,6 +9,22 @@ namespace Explorer.Services;
 
 public class FileSystemService
 {
+    private static string FormatSize(long bytes)
+    {
+        const long kb = 1024;
+        const long mb = kb * 1024;
+        const long gb = mb * 1024;
+        const long tb = gb * 1024;
+
+        return bytes switch
+        {
+            < mb => $"{bytes / (double)kb:0.#} Ko",
+            < gb => $"{bytes / (double)mb:0.#} Mo",
+            < tb => $"{bytes / (double)gb:0.#} Go",
+            _ => $"{bytes / (double)tb:0.#} To"
+        };
+    }
+
     public async Task<ICollection<FileSystemEntry>> ListEntriesAsync(string path)
     {
         return await Task.Run(() =>
@@ -34,19 +50,13 @@ public class FileSystemService
             Process.Start(new ProcessStartInfo(path) { UseShellExecute = true }));
     }
 
-    private static string FormatSize(long bytes)
+    public async Task<ICollection<DriveInfo>> ListDrivesAsync()
     {
-        const long kb = 1024;
-        const long mb = kb * 1024;
-        const long gb = mb * 1024;
-        const long tb = gb * 1024;
-
-        return bytes switch
-        {
-            < mb => $"{bytes / (double)kb:0.#} Ko",
-            < gb => $"{bytes / (double)mb:0.#} Mo",
-            < tb => $"{bytes / (double)gb:0.#} Go",
-            _ => $"{bytes / (double)tb:0.#} To"
-        };
+        return await Task.Run(() =>
+            DriveInfo.GetDrives()
+                .Where(d => d.IsReady)
+                .OrderBy(d => d.DriveType)
+                .ThenBy(d => d.Name)
+                .ToArray());
     }
 }
