d0d5d3bd151169e4ba3c3d27d5c8e193eb555d16
Michinix <michel_marcotte@orange.fr>
2026-07-03 21:49:25 +0200

    REFAC: Update HomeViewModel constructor to accept AsideLeft and NavBar view models

--- Explorer/ViewModels/HomeViewModel.cs
+++ Explorer/ViewModels/HomeViewModel.cs
@@ -1,10 +1,11 @@
-﻿using Explorer.Services;
+﻿namespace Explorer.ViewModels;
 
-namespace Explorer.ViewModels;
-
-public class HomeViewModel(NavigationService navigation, DataGridViewModel dataGrid) : ViewModelBase
+public class HomeViewModel(
+    AsideLeftViewModel asideLeft,
+    DataGridViewModel dataGrid,
+    NavBarViewModel navBar) : ViewModelBase
 {
-    public AsideLeftViewModel AsideLeft { get; } = new(navigation);
+    public AsideLeftViewModel AsideLeft { get; } = asideLeft;
     public DataGridViewModel DataGrid { get; } = dataGrid;
-    public NavBarViewModel NavBar { get; } = new(navigation, dataGrid);
+    public NavBarViewModel NavBar { get; } = navBar;
 }
