ca03f6f0aef954c49cb414e5043e8bec487bf218
Michinix <michel_marcotte@orange.fr>
2026-07-06 19:20:34 +0200

    REFAC: Move MenuFlyout to reusable control in /Controls/Primitives

---
+++ Explorer/Controls/Primitives/NewItemButton.axaml.cs
@@ -0,0 +1,31 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Explorer.Controls.Primitives;
+
+public partial class NewItemButton : UserControl
+{
+    public static readonly StyledProperty<ICommand?> CreateFolderCommandProperty =
+        AvaloniaProperty.Register<NewItemButton, ICommand?>(nameof(CreateFolderCommand));
+
+    public static readonly StyledProperty<ICommand?> CreateFileCommandProperty =
+        AvaloniaProperty.Register<NewItemButton, ICommand?>(nameof(CreateFileCommand));
+
+    public NewItemButton()
+    {
+        InitializeComponent();
+    }
+
+    public ICommand? CreateFolderCommand
+    {
+        get => GetValue(CreateFolderCommandProperty);
+        set => SetValue(CreateFolderCommandProperty, value);
+    }
+
+    public ICommand? CreateFileCommand
+    {
+        get => GetValue(CreateFileCommandProperty);
+        set => SetValue(CreateFileCommandProperty, value);
+    }
+}
