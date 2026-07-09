fea06b5418b2c40c570e9809ae1d93954f383e2c
Michinix <michel_marcotte@orange.fr>
2026-07-09 18:49:28 +0200

    REFAC: Introduce IconButton control and refactor NavBar to utilize it for navigation actions

---
+++ Explorer/Controls/Primitives/IconButton.axaml.cs
@@ -0,0 +1,31 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Explorer.Controls.Primitives;
+
+public partial class IconButton : UserControl
+{
+    public static readonly StyledProperty<string?> IconPathProperty =
+        AvaloniaProperty.Register<IconButton, string?>(nameof(IconPath));
+
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<IconButton, ICommand?>(nameof(Command));
+
+    public IconButton()
+    {
+        InitializeComponent();
+    }
+
+    public string? IconPath
+    {
+        get => GetValue(IconPathProperty);
+        set => SetValue(IconPathProperty, value);
+    }
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+}
