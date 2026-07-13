7051ad2d56734d2a4c328ae4e5cc0c242f453014
Michinix <michel_marcotte@orange.fr>
2026-07-13 14:14:34 +0200

    FEAT: Prevent window focus loss when clicking on TextBox or Button elements

--- Explorer/Views/MainWindow.axaml.cs
+++ Explorer/Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace Explorer.Views;
 
@@ -14,6 +17,9 @@ public partial class MainWindow : Window
 
     private void OnWindowPointerPressed(object? sender, PointerPressedEventArgs e)
     {
+        if (e.Source is Visual source && source.GetSelfAndVisualAncestors().Any(v => v is TextBox or Button))
+            return;
+
         var topLevel = GetTopLevel(this)!;
         topLevel.FocusManager.Focus(null);
     }
